d279ef69268163630ed2824790be8cbaa5fd9c01
baophan17 <baop99720@gmail.com>
2023-09-25 21:35:34 +0700

    get API allcode

--- src/route/web.js
+++ src/route/web.js
@@ -15,10 +15,13 @@ let initWebRoutes = (app) => {
     router.get('/delete-crud', homeController.deleteCRUD);
 
     router.post('/api/login', userController.handleLogin);
-    router.get('/api/get-all-users', userController.handleGetAllUsers)
-    router.post('/api/create-new-user', userController.handleCreateNewUser)
-    router.put('/api/edit-user', userController.handleEditUser)
-    router.delete('/api/delete-user', userController.handleDeleteUser)
+    router.get('/api/get-all-users', userController.handleGetAllUsers);
+    router.post('/api/create-new-user', userController.handleCreateNewUser);
+    router.put('/api/edit-user', userController.handleEditUser);
+    router.delete('/api/delete-user', userController.handleDeleteUser); //rest API
+
+    router.get('/allcode', userController.getAllCode)
+
     return app.use("/", router);
 }
 
