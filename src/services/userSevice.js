620a1cf5b32dcfd70aadbb37c8610cc9737da0cd
baophan17 <baop99720@gmail.com>
2023-09-29 13:56:28 +0700

    WRITE & READ FILE IMAGE with Sequelize

--- src/services/userSevice.js
+++ src/services/userSevice.js
@@ -104,6 +104,7 @@ let createNewUser = (data) => {
                     gender: data.gender,
                     roleId: data.roleId,
                     positionId: data.positionId,
+                    image: data.avatar,
 
                 })
                 resolve({
@@ -160,6 +161,10 @@ let updateUserData = (data) => {
                 user.positionId = data.positionId;
                 user.gender = data.gender;
                 user.phoneNumber = data.phoneNumber;
+                if (data.avatar) {
+                    user.image = data.avatar;
+                }
+
                 await user.save();
                 // await db.User.save({
                 //     firstName: data.firstName,
